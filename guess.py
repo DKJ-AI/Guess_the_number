08cdd7e5bb9b434cca147669f9d130e938e843bc
DIPAK JENA <dipakjena056@gmail.com>
2026-04-01 23:22:53 +0530

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+  num = random.randint(1,x)
+  guess = 0
+  while guess != num:
+    guess = int(input("enter your guess : "))
+    if guess < num:
+      print("tooo low")
+    elif guess > num:
+      print("too high")
+
+  print(f"you guessed the correct number {num}")
+
+guess(10)
